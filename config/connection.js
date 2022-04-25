57164891f969da5d4e1058c71dc115627fd0f453
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-25 18:03:34 -0500

    added server file with routes and call to open page and connection file to require in mongoose db

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const { connect, connection } = require('mongoose');
+
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
